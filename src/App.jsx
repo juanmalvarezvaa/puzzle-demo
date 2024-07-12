48c8b8be6800fffdaa274b106493c261ac74424f
Juan Alvarez <juan.alvarez@botmaker.io>
2024-07-12 16:31:39 -0300

    Add image loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [image, setImage] = useState(null);
+
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    setImage(URL.createObjectURL(file));
+  };
+
   return (
     <>
-      <p>Click on the Vite and React logos to learn more</p>
+      <header className="image-header">
+        {!image && <label htmlFor="imageInput">Please upload an image.</label>}
+        <input
+          type="file"
+          id="imageInput"
+          name="imageInput"
+          accept="image/*"
+          onChange={handleImageChange}
+        />
+        {image && (
+          <img
+            src={image}
+            alt="Loaded image"
+            style={{ heigh: "64px", width: "64px" }}
+          />
+        )}
+      </header>
     </>
   );
 }
