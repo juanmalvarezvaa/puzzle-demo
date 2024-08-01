fcf492a941ad5509ec7a6815b07ae25a8df31b0d
Juan Alvarez <juan.alvarez@botmaker.io>
2024-08-01 10:46:34 -0300

    Temporal before moving to Reorder

--- src/components/Puzzle.jsx
+++ src/components/Puzzle.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
-import { motion } from "framer-motion";
+import React, { useState, useEffect, useRef } from "react";
+import { motion, Reorder } from "framer-motion";
 import "./Puzzle.css";
 
 const PUZZLE_WIDTH = 500;
@@ -7,6 +7,10 @@ const PUZZLE_WIDTH = 500;
 const Puzzle = ({ image, rows, cols }) => {
   const [tiles, setTiles] = useState([]);
   const [solved, setSolved] = useState(false);
+  // const [pickedTile, setPickedTile] = useState();
+  // const [droppedTile, setDroppedTile] = useState();
+  // const constraintsRef = useRef(null);
+  const [items, setItems] = useState();
 
   let img = document.createElement("img");
   img.src = image;
@@ -30,10 +34,10 @@ const Puzzle = ({ image, rows, cols }) => {
         });
       }
     }
-    setTiles(shuffle(newTiles));
+    const shuffledTiles = shuffle(newTiles);
+    setTiles(shuffledTiles);
+    setItems(shuffledTiles);
   }, [image, rows, cols]);
-
-  console.log(" newTiles: ", tiles);
 
   const shuffle = (array) => {
     // Fisher-Yates shuffle algorithm (simplified)
@@ -48,26 +52,69 @@ const Puzzle = ({ image, rows, cols }) => {
     return tiles.every((tile, index) => tile.order === index);
   };
 
-  const handleTileClick = (clickedTile) => {
-    // ... (Logic to swap tiles based on user interaction)
-    // Check if the puzzle is solved after each move
-  };
+  // const handleTileClick = (clickedTile) => {
+  //   console.log("clickedTile", clickedTile);
+  //   // ... (Logic to swap tiles based on user interaction)
+  //   // Check if the puzzle is solved after each move
+  // };
+
+  // const handleTileMouseUp = (clickedTile) => {
+  //   console.log("handleTileMouseUp", clickedTile);
+  //   // ... (Logic to swap tiles based on user interaction)
+  //   // Check if the puzzle is solved after each move
+  // };
+
+  const calculateTile = (x, y) => {};
+
+  // const handleOnDragStart = (event, info, tile) => {
+  //   console.log("start: ", info.point.x, info.point.y, tile);
+  //   setPickedTile(tile.id);
+  // };
+
+  // const handleOnDragEnd = (event, info, tile) => {
+  //   console.log("end: ", info.point.x, info.point.y, tile);
+  //   calculateTile;
+  // };
+
+  console.log(" tiles: ", tiles);
 
   return (
     <div className="puzzle-container">
       {solved ? (
         <div>Congratulations! Puzzle Solved!</div>
       ) : (
-        <div className="puzzle">
+        // <motion.div className="puzzle" ref={constraintsRef}>
+        //   {tiles.map((tile) => (
+        //     <motion.div
+        //       drag
+        //       dragConstraints={constraintsRef}
+        //       dragElastic={0.1}
+        //       dragMomentum={false}
+        //       onDragStart={(event, info) =>
+        //         handleOnDragStart(event, info, tile)
+        //       }
+        //       onDragEnd={(event, info) => handleOnDragEnd(event, info, tile)}
+        //       // onMouseUp={() => handleTileMouseUp(tile)}
+        //       key={tile.id}
+        //       className="tile"
+
+        //       // onMouseDown={() => handleTileClick(tile)}
+        //       style={{ ...tile.style, aspectRatio: `${cols} / ${rows}` }}
+        //     />
+        //   ))}
+        // </motion.div>
+        <Reorder.Group axis="y" values={tiles} onReorder={setItems}>
           {tiles.map((tile) => (
-            <div
-              key={tile.id}
-              className="tile"
-              onClick={() => handleTileClick(tile)}
-              style={{ ...tile.style, aspectRatio: `${cols} / ${rows}` }}
-            />
+            <Reorder.Item key={tile.id} value={tile}>
+              <div
+                key={tile.id}
+                className="tile"
+                onClick={() => handleTileClick(tile)}
+                style={{ ...tile.style, aspectRatio: `${cols} / ${rows}` }}
+              />
+            </Reorder.Item>
           ))}
-        </div>
+        </Reorder.Group>
       )}
     </div>
   );
